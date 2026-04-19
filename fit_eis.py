d229e9b51989e40af023c370eed0aa0237d3cf86
rnegrao <renatonegraooliveira@gmail.com>
2026-04-19 16:54:32 -0300

    Refactor data files and update model parameters
    Commented out unused data files and legend entries for clarity. Updated initial parameters and bounds for model fitting.

--- fit_eis.py
+++ fit_eis.py
@@ -9,7 +9,6 @@ import matplotlib.pyplot as plt
 from lmfit import Parameters, minimize, report_fit
 from matplotlib.ticker import ScalarFormatter
 
-
 ####################################
 # Model definitions
 ####################################
@@ -44,130 +43,130 @@ def residual(params, w, data_complex):
 file_list = [
             "DATA/GDC4P5TON/TEST_01072026_GDC4P5TON_T175C_3_C01.txt",
             "DATA/GDC4P5TON/TEST_01072026_GDC4P5TON_T200C_2_C01.txt",
-            "DATA/GDC4P5TON/TEST_01072026_GDC4P5TON_T225C_2_C01.txt",
+            #"DATA/GDC4P5TON/TEST_01072026_GDC4P5TON_T225C_2_C01.txt",
             "DATA/GDC4P5TON/TEST_01072026_GDC4P5TON_T250C_2_C01.txt",
-            "DATA/GDC4P5TON/TEST_01072026_GDC4P5TON_T275C_2_C01.txt",
+            #"DATA/GDC4P5TON/TEST_01072026_GDC4P5TON_T275C_2_C01.txt",
             "DATA/GDC4P5TON/TEST_01072026_GDC4P5TON_T300C_2_C01.txt",
-            "DATA/GDC4P5TON/TEST_01072026_GDC4P5TON_T325C_2_C01.txt",
+            #"DATA/GDC4P5TON/TEST_01072026_GDC4P5TON_T325C_2_C01.txt",
             "DATA/GDC4P5TON/TEST_01072026_GDC4P5TON_T350C_2_C01.txt",
                 
             "DATA/GDC5P5TON/TEST_12092025_GDC5P5TON_T175C_2_C01.txt",
             "DATA/GDC5P5TON/TEST_12092025_GDC5P5TON_T200C_2_C01.txt",
-            "DATA/GDC5P5TON/TEST_12092025_GDC5P5TON_T225C_2_C01.txt",
+            #"DATA/GDC5P5TON/TEST_12092025_GDC5P5TON_T225C_2_C01.txt",
             "DATA/GDC5P5TON/TEST_12092025_GDC5P5TON_T250C_2_C01.txt",
-            "DATA/GDC5P5TON/TEST_12092025_GDC5P5TON_T275C_2_C01.txt",
+            #"DATA/GDC5P5TON/TEST_12092025_GDC5P5TON_T275C_2_C01.txt",
             "DATA/GDC5P5TON/TEST_12092025_GDC5P5TON_T300C_2_C01.txt",
-            "DATA/GDC5P5TON/TEST_12092025_GDC5P5TON_T325C_2_C01.txt",
+            #"DATA/GDC5P5TON/TEST_12092025_GDC5P5TON_T325C_2_C01.txt",
             "DATA/GDC5P5TON/TEST_12092025_GDC5P5TON_T350C_2_C01.txt",
 
             "DATA/GDC6P5TON/TEST_01092026_GDC6P5TON_T175C_3_C01.txt",
             "DATA/GDC6P5TON/TEST_01092026_GDC6P5TON_T200C_2_C01.txt",
-            "DATA/GDC6P5TON/TEST_01092026_GDC6P5TON_T225C_2_C01.txt",
+            #"DATA/GDC6P5TON/TEST_01092026_GDC6P5TON_T225C_2_C01.txt",
             "DATA/GDC6P5TON/TEST_01092026_GDC6P5TON_T250C_2_C01.txt",
-            "DATA/GDC6P5TON/TEST_01092026_GDC6P5TON_T275C_2_C01.txt",
+            #"DATA/GDC6P5TON/TEST_01092026_GDC6P5TON_T275C_2_C01.txt",
             "DATA/GDC6P5TON/TEST_01092026_GDC6P5TON_T300C_2_C01.txt",
-            "DATA/GDC6P5TON/TEST_01092026_GDC6P5TON_T325C_2_C01.txt",
+            #"DATA/GDC6P5TON/TEST_01092026_GDC6P5TON_T325C_2_C01.txt",
             "DATA/GDC6P5TON/TEST_01092026_GDC6P5TON_T350C_2_C01.txt",
                 
             "DATA/GDC7P2TON/TEST_01192026_GDC7P2TON_T175C_2_C01.txt",
             "DATA/GDC7P2TON/TEST_01192026_GDC7P2TON_T200C_2_C01.txt",
-            "DATA/GDC7P2TON/TEST_01192026_GDC7P2TON_T225C_2_C01.txt",
+            #"DATA/GDC7P2TON/TEST_01192026_GDC7P2TON_T225C_2_C01.txt",
             "DATA/GDC7P2TON/TEST_01192026_GDC7P2TON_T250C_2_C01.txt",
-            "DATA/GDC7P2TON/TEST_01192026_GDC7P2TON_T275C_2_C01.txt",
+            #"DATA/GDC7P2TON/TEST_01192026_GDC7P2TON_T275C_2_C01.txt",
             "DATA/GDC7P2TON/TEST_01192026_GDC7P2TON_T300C_2_C01.txt",
-            "DATA/GDC7P2TON/TEST_01192026_GDC7P2TON_T325C_2_C01.txt",
+            #"DATA/GDC7P2TON/TEST_01192026_GDC7P2TON_T325C_2_C01.txt",
             "DATA/GDC7P2TON/TEST_01192026_GDC7P2TON_T350C_2_C01.txt",
 
             "DATA/GDC8P1TON/TEST_01132026_GDC8P1TON_T175C_3_C01.txt",
             "DATA/GDC8P1TON/TEST_01132026_GDC8P1TON_T200C_2_C01.txt",
-            "DATA/GDC8P1TON/TEST_01132026_GDC8P1TON_T225C_2_C01.txt",
+            #"DATA/GDC8P1TON/TEST_01132026_GDC8P1TON_T225C_2_C01.txt",
             "DATA/GDC8P1TON/TEST_01132026_GDC8P1TON_T250C_2_C01.txt",
-            "DATA/GDC8P1TON/TEST_01132026_GDC8P1TON_T275C_2_C01.txt",
+            #"DATA/GDC8P1TON/TEST_01132026_GDC8P1TON_T275C_2_C01.txt",
             "DATA/GDC8P1TON/TEST_01132026_GDC8P1TON_T300C_2_C01.txt",
-            "DATA/GDC8P1TON/TEST_01132026_GDC8P1TON_T325C_2_C01.txt",
+            #"DATA/GDC8P1TON/TEST_01132026_GDC8P1TON_T325C_2_C01.txt",
             "DATA/GDC8P1TON/TEST_01132026_GDC8P1TON_T350C_2_C01.txt",
             
             "DATA/GDC9TON/TEST_01152026_GDC9TON_T175C_3_C01.txt",
             "DATA/GDC9TON/TEST_01152026_GDC9TON_T200C_2_C01.txt",
-            "DATA/GDC9TON/TEST_01152026_GDC9TON_T225C_2_C01.txt",
+            #"DATA/GDC9TON/TEST_01152026_GDC9TON_T225C_2_C01.txt",
             "DATA/GDC9TON/TEST_01152026_GDC9TON_T250C_2_C01.txt",
-            "DATA/GDC9TON/TEST_01152026_GDC9TON_T275C_2_C01.txt",
+            #"DATA/GDC9TON/TEST_01152026_GDC9TON_T275C_2_C01.txt",
             "DATA/GDC9TON/TEST_01152026_GDC9TON_T300C_2_C01.txt",
-            "DATA/GDC9TON/TEST_01152026_GDC9TON_T325C_2_C01.txt",
+            #"DATA/GDC9TON/TEST_01152026_GDC9TON_T325C_2_C01.txt",
             "DATA/GDC9TON/TEST_01152026_GDC9TON_T350C_2_C01.txt",
 
             "DATA/GDC9P9TON/TEST_01222026_GDC9P9TON_T175C_2_C01.txt",
             "DATA/GDC9P9TON/TEST_01222026_GDC9P9TON_T200C_2_C01.txt",
-            "DATA/GDC9P9TON/TEST_01222026_GDC9P9TON_T225C_2_C01.txt",
+            #"DATA/GDC9P9TON/TEST_01222026_GDC9P9TON_T225C_2_C01.txt",
             "DATA/GDC9P9TON/TEST_01222026_GDC9P9TON_T250C_2_C01.txt",
-            "DATA/GDC9P9TON/TEST_01222026_GDC9P9TON_T275C_2_C01.txt",
+            #"DATA/GDC9P9TON/TEST_01222026_GDC9P9TON_T275C_2_C01.txt",
             "DATA/GDC9P9TON/TEST_01222026_GDC9P9TON_T300C_2_C01.txt",
-            "DATA/GDC9P9TON/TEST_01222026_GDC9P9TON_T325C_2_C01.txt",
+            #"DATA/GDC9P9TON/TEST_01222026_GDC9P9TON_T325C_2_C01.txt",
             "DATA/GDC9P9TON/TEST_01222026_GDC9P9TON_T350C_2_C01.txt"
             ]
 
 legend_string = [
             "GDC 4.5TON T=175C",
             "GDC 4.5TON T=200C",
-            "GDC 4.5TON T=225C",
+            #"GDC 4.5TON T=225C",
             "GDC 4.5TON T=250C",
-            "GDC 4.5TON T=275C",
+            #"GDC 4.5TON T=275C",
             "GDC 4.5TON T=300C",
-            "GDC 4.5TON T=325C",
+            #"GDC 4.5TON T=325C",
             "GDC 4.5TON T=350C",
                 
             "GDC 5.5TON T=175C",
             "GDC 5.5TON T=200C",
-            "GDC 5.5TON T=225C",
+            #"GDC 5.5TON T=225C",
             "GDC 5.5TON T=250C",
-            "GDC 5.5TON T=275C",
+            #"GDC 5.5TON T=275C",
             "GDC 5.5TON T=300C",
-            "GDC 5.5TON T=325C",
+            #"GDC 5.5TON T=325C",
             "GDC 5.5TON T=350C",
 
             "GDC 6.5TON T=175C",
             "GDC 6.5TON T=200C",
-            "GDC 6.5TON T=225C",
+            #"GDC 6.5TON T=225C",
             "GDC 6.5TON T=250C",
-            "GDC 6.5TON T=275C",
+            #"GDC 6.5TON T=275C",
             "GDC 6.5TON T=300C",
-            "GDC 6.5TON T=325C",
+            #"GDC 6.5TON T=325C",
             "GDC 6.5TON T=350C",
                 
             "GDC 7.2TON T=175C",
             "GDC 7.2TON T=200C",
-            "GDC 7.2TON T=225C",
+            #"GDC 7.2TON T=225C",
             "GDC 7.2TON T=250C",
-            "GDC 7.2TON T=275C",
+            #"GDC 7.2TON T=275C",
             "GDC 7.2TON T=300C",
-            "GDC 7.2TON T=325C",
+            #"GDC 7.2TON T=325C",
             "GDC 7.2TON T=350C",
 
             "GDC 8.1TON T=175C",
             "GDC 8.1TON T=200C",
-            "GDC 8.1TON T=225C",
+            #"GDC 8.1TON T=225C",
             "GDC 8.1TON T=250C",
-            "GDC 8.1TON T=275C",
+            #"GDC 8.1TON T=275C",
             "GDC 8.1TON T=300C",
-            "GDC 8.1TON T=325C",
+            #"GDC 8.1TON T=325C",
             "GDC 8.1TON T=350C",
-            
+
             "GDC 9.0TON T=175C",
             "GDC 9.0TON T=200C",
-            "GDC 9.0TON T=225C",
+            #"GDC 9.0TON T=225C",
             "GDC 9.0TON T=250C",
-            "GDC 9.0TON T=275C",
+            #"GDC 9.0TON T=275C",
             "GDC 9.0TON T=300C",
-            "GDC 9.0TON T=325C",
+            #"GDC 9.0TON T=325C",
             "GDC 9.0TON T=350C",
 
             "GDC 9.9TON T=175C",
             "GDC 9.9TON T=200C",
-            "GDC 9.9TON T=225C",
+            #"GDC 9.9TON T=225C",
             "GDC 9.9TON T=250C",
-            "GDC 9.9TON T=275C",
+            #"GDC 9.9TON T=275C",
             "GDC 9.9TON T=300C",
-            "GDC 9.9TON T=325C",
+            #"GDC 9.9TON T=325C",
             "GDC 9.9TON T=350C"
             ]
 
@@ -181,6 +180,52 @@ hex_colors = [
             "#FCBBA1",
             "#FEE0D2"
             ]
+
+
+input_parameters=[
+            [10,297000,1.5100e-10,0.85,36629,14.96E-9,1.1,35429400,91796],
+            [10,84558,0.235e-9,0.85,8000,90e-09,1.0,45000000,50000],
+            [10,10005,0.35e-9,0.85,500,150e-09,0.95,40000000,75000],
+            [0,1750,0.35e-9,0.99,100.0,1e-12,1.0,4000000,55000],
+            [0,510,1.0e-11,0.99,0.1,1e-12,1.0,4000000,55000],
+
+            [10,297000,1.5100e-10,0.85,36629,14.96E-9,1.1,35429400,91796],
+            [10,84558,0.235e-9,0.85,8000,90e-09,1.0,45000000,50000],
+            [10,10005,0.35e-9,0.85,500,150e-09,0.95,40000000,75000],
+            [0,1750,0.35e-9,0.99,100.0,1e-12,1.0,4000000,55000],
+            [0,510,1.0e-11,0.99,0.1,1e-12,1.0,4000000,55000],
+
+            [10,297000,1.5100e-10,0.85,36629,14.96E-9,1.1,35429400,91796],
+            [10,84558,0.235e-9,0.85,8000,90e-09,1.0,45000000,50000],
+            [10,10005,0.35e-9,0.85,500,150e-09,0.95,40000000,75000],
+            [0,1750,0.35e-9,0.99,100.0,1e-12,1.0,4000000,55000],
+            [0,510,1.0e-11,0.99,0.1,1e-12,1.0,4000000,55000],
+
+            [10,297000,1.5100e-10,0.85,36629,14.96E-9,1.1,35429400,91796],
+            [10,84558,0.235e-9,0.85,8000,90e-09,1.0,45000000,50000],
+            [10,10005,0.35e-9,0.85,500,150e-09,0.95,40000000,75000],
+            [0,1750,0.35e-9,0.99,100.0,1e-12,1.0,4000000,55000],
+            [0,510,1.0e-11,0.99,0.1,1e-12,1.0,4000000,55000],
+
+            [10,297000,1.5100e-10,0.85,36629,14.96E-9,1.1,35429400,91796],
+            [10,84558,0.235e-9,0.85,8000,90e-09,1.0,45000000,50000],
+            [10,10005,0.35e-9,0.85,500,150e-09,0.95,40000000,75000],
+            [0,1750,0.35e-9,0.99,100.0,1e-12,1.0,4000000,55000],
+            [0,510,1.0e-11,0.99,0.1,1e-12,1.0,4000000,55000],
+
+            [10,297000,1.5100e-10,0.85,36629,14.96E-9,1.1,35429400,91796],
+            [10,84558,0.235e-9,0.85,8000,90e-09,1.0,45000000,50000],
+            [10,10005,0.35e-9,0.85,500,150e-09,0.95,40000000,75000],
+            [0,1750,0.35e-9,0.99,100.0,1e-12,1.0,4000000,55000],
+            [0,510,1.0e-11,0.99,0.1,1e-12,1.0,4000000,55000],
+
+            [10,297000,1.5100e-10,0.85,36629,14.96E-9,1.1,35429400,91796],
+            [10,84558,0.235e-9,0.85,8000,90e-09,1.0,45000000,50000],
+            [10,10005,0.35e-9,0.85,500,150e-09,0.95,40000000,75000],
+            [0,1750,0.35e-9,0.99,100.0,1e-12,1.0,4000000,55000],
+            [0,510,1.0e-11,0.99,0.1,1e-12,1.0,4000000,55000]
+            ]
+
 j=0
 
 for i in file_list:
@@ -200,37 +245,37 @@ for i in file_list:
         # FREQUENCY ASSUMPTION
         ####################################
         # If you have the real frequencies, replace this vector with them (one value per point).
-        f_max = 7e6
-        f_min = 1e0
+        f_max = 3e6
+        f_min = 1.e0
         freqs = np.logspace(np.log10(f_max), np.log10(f_min), N)
         omega = 2*np.pi*freqs
 
         ####################################
         # Initial parameters & bounds
         ####################################
-        n=1.
-        R0=10
-        R1=86558
-        Q1=0.235E-9
-        n1=0.85
-        R2=15000
-        Q2=55E-9
-        n2=0.95
-        Rd=54000000
-        td=62700
+        n=0.1
+        R0=input_parameters[j][0]
+        R1=input_parameters[j][1]
+        Q1=input_parameters[j][2]
+        n1=input_parameters[j][3]
+        R2=input_parameters[j][4]
+        Q2=input_parameters[j][5]
+        n2=input_parameters[j][6]
+        Rd=input_parameters[j][7]
+        td=input_parameters[j][8]
 
         params = Parameters()
 
         #resistance
         params.add('R0', value=R0, vary=False)
         #first circle
-        params.add('R1', value=R1, min=R1-n*R1, max=R1+n*R1, vary=True)
-        params.add('Q1', value=Q1, min=Q1-n*Q1, max=Q1+n*Q1, vary=True)
-        params.add('n1', value=n1, min=n1-n*n1, max=n1+n*n1, vary=True)
+        params.add('R1', value=R1, min=R1-n*R1, max=R1+n*R1, vary=False)
+        params.add('Q1', value=Q1, min=Q1-n*Q1, max=Q1+n*Q1, vary=False)
+        params.add('n1', value=n1, min=n1-n*n1, max=n1+n*n1, vary=False)
         #second circle
-        params.add('R2', value=R2, min=R2-n*R2, max=R2+n*R2, vary=True)
-        params.add('Q2', value=Q2, min=Q2-n*Q2, max=Q2+n*Q2, vary=True)
-        params.add('n2', value=n2, min=n2-n*n2, max=n2+n*n2, vary=True)
+        params.add('R2', value=R2, min=R2-n*R2, max=R2+n*R2, vary=False)
+        params.add('Q2', value=Q2, min=Q2-n*Q2, max=Q2+n*Q2, vary=False)
+        params.add('n2', value=n2, min=n2-n*n2, max=n2+n*n2, vary=False)
         #diffusion
         params.add('Rd', value=Rd, min=Rd-n*Rd, max=Rd+n*Rd, vary=False)
         params.add('td', value=td, min=td-n*td, max=td+n*td, vary=False)
@@ -251,7 +296,7 @@ for i in file_list:
         
         figfile = i+'.png'
         fig, ax = plt.subplots(figsize=(8, 8))
-        plt.plot(Re, Im, 'o', label='EIS Measurements '+legend_string[j], markersize=4,color=hex_colors[1])
+        plt.plot(Re, Im, 'o', label='EIS Measurements '+i, markersize=4,color=hex_colors[1])
         j=j+1
         plt.plot(Z_fit.real, -Z_fit.imag, '--', label='Equivalent Circuit Model', linewidth=1)
         plt.xlabel('Re(Z) / Ohm')
